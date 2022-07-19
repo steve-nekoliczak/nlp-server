a1c659bc73c0a020a350a97107fc22f847ec19ad
steve-nekoliczak <steve.nekoliczak@protonmail.com>
2022-07-18 21:23:14 -0500

    add basic working de processor function
    will use as a template for later additions

--- app.py
+++ app.py
@@ -1,11 +1,17 @@
 # flask_web/app.py
 
 from flask import Flask
+from de_processor import DeProcessor
+
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
     return 'Hey, we have Flask in a Docker container!'
+
+@app.route('/de/sentence')
+def de_process_sentence():
+    return DeProcessor.process_sentence("Ich bin Berliner.")
 
 
 if __name__ == '__main__':
--- de_processor.py
+++ de_processor.py
@@ -1,1 +1,10 @@
 import stanza
+import json
+
+
+class DeProcessor():
+    nlp = stanza.Pipeline('de')
+
+    @staticmethod
+    def process_sentence(sentence):
+        return json.dumps(DeProcessor.nlp(sentence).to_dict())
